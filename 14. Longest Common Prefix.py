b81e4365198400e66601d51c347433767a8ec1ad
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2019-01-27 11:04:05 -0500

    Create 14. Longest Common Prefix.py

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,10 @@
+#by using order string
+def longestCommonPrefix(strs):
+    if not strs: return ''
+    s1 = min(strs)
+    s2 = max(strs)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i]
+    return s1
+                
