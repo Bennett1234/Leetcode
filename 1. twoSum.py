415c8287996da2441fd9cb43f2b8d455d872e587
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2019-10-09 15:47:18 -0400

    Update 1. twoSum.py

--- 1. twoSum.py
+++ 1. twoSum.py
@@ -1,13 +1,5 @@
 #Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 #You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-def twoSum(numbers,target):
-    for i,a in enumerate(numbers):
-        for j, b in enumerate(numbers[i+1:]):
-            if a + b == target:
-                return [i,j+i+1]
-    return [-1,-1]
-
 def TwoSum (l, target):
     for i in range(0,len(l)):
         for j in range(i+1,len(l)):
