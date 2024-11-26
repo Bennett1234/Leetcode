faeadbdc1764717c97d7f03fc38ad44b07c56567
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2024-11-25 23:10:03 -0500

    Update 9. Palindrome Number (faster than 73.61%).py

--- 9. Palindrome Number (faster than 73.61%).py
+++ 9. Palindrome Number (faster than 73.61%).py
@@ -1,2 +1,20 @@
 def isPalindrome(x):
     return str(x)==str(x)[::-1]
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+        if x %10 == 0:
+            return False
+        reverse = 0
+        orig = x
+        while x > 0:
+            last = x % 10
+            reverse = reverse*10+last
+            x = x // 10
+        return orig == reverse
+        
